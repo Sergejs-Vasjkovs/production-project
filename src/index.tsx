76b51fde1966e703548cc655e8de87b0ae676128
Sergejs-Vasjkovs <s.vasjkovs@gmail.com>
2024-09-20 13:35:09 +0300

    add modal, portal, change header

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,13 @@ import App from "./app/App";
 import "./shared/config/i18n/i18n";
 import "./app/styles/index.scss";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "./app/providers/ThemeProvider";
+import { Theme, ThemeProvider } from "./app/providers/ThemeProvider";
 import { ErrorBoundary } from "./app/providers/ErrorBoundary";
 
 render(
     <BrowserRouter>
         <ErrorBoundary>
-            <ThemeProvider>
+            <ThemeProvider initialTheme={Theme.LIGHT}>
                 <App />
             </ThemeProvider>
         </ErrorBoundary>
